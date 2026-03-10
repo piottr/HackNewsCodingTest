a3d4af4b495c362246d81fe12e4dcec4b44b9d7e
piottr <piottr.k@gmail.com>
2026-03-10 11:49:04 +0100

    feat(#coding-test): refactoring BestStoriesService

--- HackerNews.API/Services/BestStoriesService.cs
+++ HackerNews.API/Services/BestStoriesService.cs
@@ -1,5 +1,6 @@
 ﻿using HackerNews.API.Client.Interfaces;
 using HackerNews.API.Dto;
+using HackerNews.API.Models;
 using HackerNews.API.Services.Interfaces;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -20,43 +21,52 @@ public class BestStoriesService: IBestStoriesService
     {
         var cacheKey = $"BestStories_{n}";
 
-        if (!_cache.TryGetValue(cacheKey, out List<StoryDto>? stories))
+        if (!_cache.TryGetValue(cacheKey, out List<StoryDto>? cachedStories))
         {
             var ids = await _client.GetBestStoryIdsAsync();
+            var items = await FetchItemsWithLimitAsync(ids.Take(n), 10);
 
-            var semaphore = new SemaphoreSlim(10);
-            var tasks = ids.Take(n).Select(async id =>
-            {
-                await semaphore.WaitAsync();
-                try
-                {
-                    return await _client.GetItemAsync(id);
-                }
-                finally
-                {
-                    semaphore.Release();
-                }
-            });
-
-            var items = await Task.WhenAll(tasks);
-
-            stories = items
+            cachedStories = items
                 .Where(x => x != null)
-                .Select(x => new StoryDto
-                {
-                    Title = x!.Title,
-                    Uri = x!.Url,
-                    PostedBy = x!.By,
-                    Time = DateTimeOffset.FromUnixTimeSeconds(x.Time ?? 0),
-                    Score = x.Score ?? 0,
-                    CommentCount = x.Descendants ?? 0
-                })
+                .Select(MapToStoryDto)
                 .OrderByDescending(x => x.Score)
                 .ToList();
 
-            _cache.Set(cacheKey, stories, TimeSpan.FromMinutes(5));
+            _cache.Set(cacheKey, cachedStories, TimeSpan.FromMinutes(5));
         }
 
-        return (stories ?? Enumerable.Empty<StoryDto>()).ToList();
+        return cachedStories ?? new List<StoryDto>();
     }
+
+    private async Task<List<HackerNewsItem>> FetchItemsWithLimitAsync(IEnumerable<int> ids, int maxConcurrency)
+    {
+        var semaphore = new SemaphoreSlim(maxConcurrency);
+        var tasks = ids.Select(async id =>
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                return await _client.GetItemAsync(id);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        });
+
+        return (await Task.WhenAll(tasks)).ToList();
+    }
+
+    private StoryDto MapToStoryDto(HackerNewsItem item)
+    {
+        return new StoryDto
+        {
+            Title = item.Title,
+            Uri = item.Url,
+            PostedBy = item.By,
+            Time = DateTimeOffset.FromUnixTimeSeconds(item.Time ?? 0),
+            Score = item.Score ?? 0,
+            CommentCount = item.Descendants ?? 0
+        };
+    } 
 }
